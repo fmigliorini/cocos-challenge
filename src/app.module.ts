fead1aa8cde6144dcfc7e106bb70e41df1758d7f
fmigliorini <facundo.migliorini@gmail.com>
2025-10-21 23:59:38 -0300

    Adding Logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PortfolioController } from './modules/portfolio/portfolio.controller';
 import { PortfolioService } from './modules/portfolio/portfolio.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoggingModule } from './infrastructure/logger/logging.module';
 
 @Module({
-  imports: [],
+  // Load environment variables from .env file
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    // Logging module
+    LoggingModule,
+  ],
   controllers: [AppController, PortfolioController],
   providers: [AppService, PortfolioService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -36,6 +37,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.useLogger(app.get(Logger));
 
   await app.listen(process.env.PORT ?? 3000);
 }
