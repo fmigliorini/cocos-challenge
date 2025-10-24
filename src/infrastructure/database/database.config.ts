ae6c58c086e64a486e5acdccd4d04dd67a4e9e1e
fmigliorini <facundo.migliorini@gmail.com>
2025-10-24 14:54:14 -0300

    Improving database connection

--- src/infrastructure/database/database.config.ts
+++ src/infrastructure/database/database.config.ts
@@ -4,11 +4,16 @@ import { ConfigService } from '@nestjs/config';
 export const databaseConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => {
+  const isProduction = configService.get<string>('DATABASE_SSL') === 'PROD';
+
   return {
     type: 'postgres',
     url: configService.get<string>('DATABASE_URL'),
     autoLoadEntities: true,
     synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'),
     logging: configService.get<boolean>('DATABASE_LOGGING'),
+    // SSL configuration for secure connections
+    // Database provided by Cocos contains SSL but locally is not required :)
+    ssl: isProduction ? true : false,
   };
 };
